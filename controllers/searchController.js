9c96f7d3e2fc9eeaeeef3e525c2b9c3450d4059c
branquinhoaa <amanda.alves15@gmail.com>
2016-08-10 10:39:37 -0300

    search adequations

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,5 +1,6 @@
 var Yelp=require('../yelpAPI.js');
 var model=require('../models/Models.js');
+var flash=require('../utilities/flash.js').flash;
 
 var options ={
   consumer_key: process.env.YELPS_CONSUMERKEY,
@@ -23,26 +24,32 @@ module.exports = {
       location: location
     }, function(err,data){
       if(err){
+        flash(req, 'danger', 'Error!', 'An error occurred in your search, try again.');
         res.render('home/error',{error: err});
       } 
-      var myId=req.session.userId; 
-      var bars = data['businesses'];
-      model.bargoingsModel.countUsergoings(function(err, bargoingData){
-        find(myId, function(err, barsImGoing){
-          if (err){ res.render('home/error',{error: err});}
-          else {
-            for (var i in bars){
-              var userGoing =bargoingData.find(function(userGoing){
-                return (bars[i]['id']==userGoing['_id']);
-              });
-              bars[i].usersGoing= userGoing ? userGoing.count : 0;
-              bars[i].imGoing = (barsImGoing.indexOf(bars[i].id)!==-1);
+      if(!data){
+        flash(req, 'danger', 'Error!', 'Place not found.');
+        res.redirect('/');
+      }else{
+        var myId=req.session.userId; 
+        var bars = data['businesses'];
+        model.bargoingsModel.countUsergoings(function(err, bargoingData){
+          find(myId, function(err, barsImGoing){
+            if (err){ res.render('home/error',{error: err});}
+            else {
+              for (var i in bars){
+                var userGoing =bargoingData.find(function(userGoing){
+                  return (bars[i]['id']==userGoing['_id']);
+                });
+                bars[i].usersGoing= userGoing ? userGoing.count : 0;
+                bars[i].imGoing = (barsImGoing.indexOf(bars[i].id)!==-1);
+              }
+              res.render('night-events/index', {bars: bars});     
             }
-            res.render('night-events/index', {bars: bars});     
-          }
-        })     
-      })   
-    })
+          })     
+        })   
+      }  
+    })     
   }
 }
 
